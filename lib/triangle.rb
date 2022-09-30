6099ad0271e6c4b54bb2fd9b58fdb760d9b2a819
Raúl Reymundi <raul@kickstarter.com>
2022-09-30 17:27:30 -0400

    commit tree 3a2e34446aa01ee96a9753716fbdac82838cc4b8

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind 
+    validate_triangle
+    if side1 == side2 && side1 == side3
+      :equilateral
+    elsif side1 == side2 || side1 == side3 || side2 == side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_greater_than_zero?
+    [side1, side2, side3].all?(&:positive?)
+  end
+
+  def valid_triangle_inequality?
+    side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
+  end 
+
 end
